38c32483cf2a8fbbab10c8da91279875af0b78f4
Michael Ganesan <cookieganesan@gmail.com>
2018-05-09 16:09:04 -0400

    Updated renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,4 +1,3 @@
-import { Menu } from 'electron';
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
